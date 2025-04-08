c0d7043a9a09f08a3791f11e36f66ef6f7f9a7f0
David Melara <davidsal1995@gmail.com>
2025-04-08 10:20:25 -0600

    chore: initial commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,36 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
+import AppContainer from '@components/appContainer';
 import './global.css';
+import SplashScreen from '@screens/splashScreen';
+import { useFonts } from 'expo-font';
+import { useEffect, useState } from 'react';
+import { Text } from 'react-native';
+import ScreenContainer from '@components/screenContainer';
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  const [fontsLoaded] = useFonts({
+    'Poppins-Light': require('@assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Regular': require('@assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Medium': require('@assets/fonts/Poppins-Medium.ttf'),
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsReady(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
-    </>
+    <AppContainer>
+      {fontsLoaded && isReady === true ? (
+        <ScreenContainer>
+          <Text className="font-poppins">Country Picker</Text>
+        </ScreenContainer>
+      ) : (
+        <SplashScreen />
+      )}
+    </AppContainer>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  content: ['./App.{js,ts,tsx}', './src/**/*.{js,ts,tsx}'],
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins-light': ['Poppins-Light', 'sans-serif'],
+        poppins: ['Poppins-Regular', 'sans-serif'],
+        'poppins-bold': ['Poppins-Medium', 'sans-serif'],
+      },
+      colors: {
+        background: {
+          DEFAULT: '#ffffff',
+          dark: '#181b32',
+        },
+        onBackground: {
+          DEFAULT: '#181b32',
+          dark: '#ffffff',
+        },
+        primary: {
+          DEFAULT: '#4b22f4',
+          dark: '#4b22f4',
+        },
+      },
+    },
   },
+  darkMode: 'media',
   plugins: [],
 };
